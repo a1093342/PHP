6a267ffbc4f5b823ac0144b5dd9d3fdef263e417
a1093342 <100509587+a1093342@users.noreply.github.com>
2022-03-16 16:20:25 +0800

    Add files via upload

---
+++ googleform.php
@@ -0,0 +1,44 @@
+<html>
+
+<head></head>
+
+<title>墾丁三日游</title>
+
+<body bgcolor='#DC96F5'>
+
+<center><H1>墾丁三日游</H1></center>
+
+<OL>
+<LI>Please choose your favourite design<br/>
+<a href='http://www.nuk.edu.tw'><img src='shirt1.jpg' title='Design 1' width='20%'/></a>
+<a href='http://www.nuk.edu.tw'><img src='shirt2.jpg' title='Design 2' width='20%'/><br/></a>
+
+<br/><LI>Name:<input type="text" name="Uname"><br>
+Email:<input type="email" name="Uname"><br/>
+Telephone number:<input type="text" name="Uname"><br/>
+Gender:<input type='radio' name='Gender'>Male<input type='radio' name='Gender'>Female<br/>
+Food Preference:<input type='checkbox' name='Chicken'>Chicken<input type='checkbox' name='Beef'>Beef<input type='checkbox' name='Pork'>Pork<input type='checkbox' name='Seafoood'>Seafood<input type='checkbox' name='Vegetarian'>Vegetarian<br/>
+T-shirt size:<select name='city' multiple>
+    <option>S(Small)</option>
+    <option>M(Medium)</option>
+    <option>L(large)</option>
+    <option>XL(Extra Large)</option>
+</select><br/>
+T-shirt color:<input type='color' name='color'><br/>
+Your birthday:<input type='date' name='date'><br/>
+Number of tickets:<input type='number' name='ticket'><br/>
+
+<br><table border='2' width='30%'>
+
+<tr><th></th><th>S</th><th>M</th><th>L</th><th>XL</th></tr>
+<tr><td>chest</td><td>36</td><td>38</td><td>40</td><td>42</td></tr>
+<tr><td>width</td><td>38</td><td>40</td><td>42</td><td>44</td></tr>
+<tr><td>length</td><td>25</td><td>26</td><td>27</td><td>28</td></tr>
+
+</table>
+
+<center><br/><a href='http://www.nuk.edu.tw'><input type="submit"></a><input type="reset"><br/></center>
+</OL>
+
+</body>
+</html>
